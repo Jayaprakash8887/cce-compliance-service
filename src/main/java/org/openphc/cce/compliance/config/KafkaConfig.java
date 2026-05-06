08c99811cb1f6cafe5aae0c132efe34373ce1d8d
Jayaprakash N <jayaprakashn@medtechsol.com>
2026-05-06 18:26:35 +0530

    feat: disable type info headers in JsonSerializer for Kafka producer

--- src/main/java/org/openphc/cce/compliance/config/KafkaConfig.java
+++ src/main/java/org/openphc/cce/compliance/config/KafkaConfig.java
@@ -79,6 +79,7 @@ public class KafkaConfig {
         Map<String, Object> props = new HashMap<>(kafkaProperties.buildProducerProperties(null));
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
         return new DefaultKafkaProducerFactory<>(props);
     }
 
